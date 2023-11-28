d3f0179157a8f6a81afa2ae37aaf4212d8864eca
devops-abdullah <66333235+devops-abdullah@users.noreply.github.com>
2023-11-28 18:53:07 +0500

    Update rsync_with_credential_and_time.sh
    Error Fix in rsync script

--- rsync_with_credential_and_time.sh
+++ rsync_with_credential_and_time.sh
@@ -6,7 +6,8 @@ DEST_USER="username"
 DEST_HOST="1.1.1.1"
 DEST_MODULE="Rec"
 PASSWORD='123123123123'
-THREE_MONTHS_AGO=$(date -d "3 months ago" +%s)  # Get timestamp for three months ago
+THREE_MONTHS_AGO=$(date -d "3 months ago" +%s)  # Get timestamp for three months ago ### This option is also use able.
+OLD_DIRECTORIES=$(find "$SOURCE_DIR" -type d -mtime +90)
 PID="/var/run/rsync_sh.pid"
 
 if [ -f $PID ]; then
@@ -23,8 +24,11 @@ else
                 chmod 600 "$password_file"
 
                 # Run rsync with the password file and only sync files older than three months
-                rsync -avz --bwlimit=1000 --password-file="$password_file" --update --compare-dest="$SOURCE_DIR" --files-from=<(find "$SOURCE_DIR" -type f -mtime +"$(($THREE_MONTHS_AGO/(60*60*24)))") "$SOURCE_DIR" "$DEST_USER@$DEST_HOST::$DEST_MODULE"
-
+                for DIR in $OLD_DIRECTORIES; do
+                        cd $DIR && pwd \
+                        && rsync -avz --bwlimit=1000 --password-file="$password_file" "$DIR" "$DEST_USER@$DEST_HOST::$DEST_MODULE"
+                done
+                
                 # Clean up the temporary password file
                 rm -f "$password_file"
 
